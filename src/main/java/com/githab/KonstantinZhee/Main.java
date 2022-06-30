f2bc227f5fcd65cc0815d9469b772a6b8ee5d64c
”Konstantin_Zhe” <konstantin.zheltukhin@devim.team>
2022-06-30 12:10:29 +0300

    some optimization

--- src/main/java/com/githab/KonstantinZhee/Main.java
+++ src/main/java/com/githab/KonstantinZhee/Main.java
@@ -1,15 +1,12 @@
 package com.githab.KonstantinZhee;
 
-import com.githab.KonstantinZhee.DAO.GoogleSheets;
 import com.githab.KonstantinZhee.service.Service;
-import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.model.ValueRange;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.List;
+
 
 
 public class Main {
