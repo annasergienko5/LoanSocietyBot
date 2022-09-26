37474c574b41e0d4bd61bd24aaf034d4bb8bfde9
KonstantinZhee <konstantin4ik33@gmail.com>
2022-09-26 12:46:48 +0000

    Resolve txt fullsearch

--- src/main/java/me/staff4/GringottsTool/Converters/ConverterTxt.java
+++ src/main/java/me/staff4/GringottsTool/Converters/ConverterTxt.java
@@ -1,19 +1,30 @@
 package me.staff4.GringottsTool.Converters;
 import me.staff4.GringottsTool.Constants;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ConverterTxt {
-    public final String toTxtFile(final String text) throws IOException {
+    public final String saveToTxtFile(final String text) throws IOException {
         Path tempPath;
+        Writer out = null;
         try {
             tempPath = Files.createTempFile(null, ".txt");
-            Files.write(tempPath, text.getBytes());
+            out = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(tempPath.toFile()), StandardCharsets.UTF_8));
+            out.write(text);
         } catch (IOException | RuntimeException e) {
             e.printStackTrace();
             throw new IOException(Constants.ERROR_WRITING_TXT_FILE);
+        } finally {
+            assert out != null;
+            out.close();
         }
         return tempPath.toString();
     }
